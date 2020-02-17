29bb956956bf1e6baf9f852aef0114382edcfdc4
Antonia Isabel Hernandez Castaño <ahercast@everis.com>
2020-02-17 17:35:46 +0100

    my first project commit

---
+++ tests/calculator.js
@@ -0,0 +1,35 @@
+let homepage = require('../pages/homepage');
+let fs = require('fs');  //para hacer capturas en un paso concreto
+
+describe('Demo calculator tests', function(){
+    
+    it('addition test', function(){
+        //browser.get('http://juliemr.github.io/protractor-demo/');
+        homepage.get('http://juliemr.github.io/protractor-demo/');
+     
+        //element(by.xpath("//*[@ng-model='first']")).sendKeys('2');
+        //element(by.model('first')).sendKeys('2');
+        homepage.enterFirstNumber('4');
+         
+        //element(by.model('second')).sendKeys('3');
+        homepage.enterSecondNumber('3');
+
+        //se hace una captura en un sitio concreto, aquí se haría después de introducir el numero 3
+        browser.takeScreenshot().then(function(fullPage){
+            var stream = fs.createWriteStream ('./target/fullpage.png');
+            stream.write(new  Buffer(fullPage,'base64' ));
+            stream.end();
+        });        
+
+        //element(by.css('[ng-click="doAddition()"]')).click();
+        homepage.clickGo();
+
+        //let result= element(by.cssContainingText('.ng-binding','5'));
+        //expect(result.getText()).toEqual('5');
+        homepage.verifyResult('7');
+        
+        browser.sleep(2000);
+    
+    });
+  
+});
